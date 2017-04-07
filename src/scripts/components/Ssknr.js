b960b530afaff444901e81520384c3ef1b9f6c0a
Filose <jack.filose@thisisdare.com>
2017-04-07 11:19:14 +0100

    removed junk

--- src/scripts/components/Ssknr.js
+++ src/scripts/components/Ssknr.js
@@ -60,40 +60,4 @@ class Ssknr extends React.Component {
   }
 }
 
-// const Ssknr = (props) => {
-//   const { elems, id } = props;
-//   const currentElem = elems[id];
-//   function handleToggle(e) {
-//     e.preventDefault();
-//     const { closeSsknr, openSsknr } = props;
-//     if (elems[id].open) {
-//       closeSsknr(id);
-//     } else {
-//       openSsknr(id);
-//     }
-//   }
-//   const ssknrContentOpen = currentElem.open ? ` ${style.ssknrContentOpen}` : '';
-//   const selectedOption = currentElem.options.find(o => o.selected);
-//   const title = selectedOption ? selectedOption.text : currentElem.title;
-//   return (
-//     <dl className={style.ssknr} onClick={handleToggle}>
-//       <dt>{title}</dt>
-//       <dd className={`${style.ssknrContent}${ssknrContentOpen}`}>
-//         <ul className={style.ssknrOptions}>
-//           {currentElem.options.map((option, i) => {
-//             return (
-//               <SsknrOption
-//                 key={`item-${option.index}`}
-//                 option={option}
-//                 rootId={id}
-//                 {...props}
-//               />
-//             );
-//           })}
-//         </ul>
-//       </dd>
-//     </dl>
-//   );
-// };
-
 export default Ssknr;
